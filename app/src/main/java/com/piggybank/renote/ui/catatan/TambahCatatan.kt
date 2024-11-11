8096f1b1704a71e6c3f39cb16e159c7f0c6cab32
Muhammad Fahreza <yya77591@gmail.com>
2024-11-11 13:54:12 +0700

    delete nav view in TambahCatatan

--- app/src/main/java/com/piggybank/renote/ui/catatan/TambahCatatan.kt
+++ app/src/main/java/com/piggybank/renote/ui/catatan/TambahCatatan.kt
@@ -5,6 +5,9 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.piggybank.renote.R
 import com.piggybank.renote.databinding.FragmentTambahBinding
 
 class TambahCatatan : Fragment() {
@@ -18,11 +21,24 @@ class TambahCatatan : Fragment() {
         savedInstanceState: Bundle?
     ): View {
         _binding = FragmentTambahBinding.inflate(inflater, container, false)
+
+        val bottomNavigationView = requireActivity().findViewById<BottomNavigationView>(R.id.nav_view)
+
+        bottomNavigationView?.visibility = View.GONE
+
+        binding.iconBack.setOnClickListener {
+            findNavController().navigate(R.id.navigation_catatan)
+        }
+
         return binding.root
     }
 
     override fun onDestroyView() {
         super.onDestroyView()
+
+        val bottomNavigationView = requireActivity().findViewById<BottomNavigationView>(R.id.nav_view)
+        bottomNavigationView?.visibility = View.VISIBLE
+
         _binding = null
     }
 }
