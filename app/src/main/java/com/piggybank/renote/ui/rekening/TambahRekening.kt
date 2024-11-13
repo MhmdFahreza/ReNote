84bef1e753981b7f9c0aae87b7af8a449c22e554
Muhammad Fahreza <yya77591@gmail.com>
2024-11-13 10:27:14 +0700

    Fontend update

--- app/src/main/java/com/piggybank/renote/ui/rekening/TambahRekening.kt
+++ app/src/main/java/com/piggybank/renote/ui/rekening/TambahRekening.kt
@@ -1,10 +1,21 @@
 package com.piggybank.renote.ui.rekening
 
+import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
 import com.piggybank.renote.R
 
 class TambahRekening : Fragment(R.layout.fragment_tambah_rekening) {
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        // Set OnClickListener untuk icon_back
+        view.findViewById<View>(R.id.icon_back).setOnClickListener {
+            findNavController().navigateUp()
+        }
+    }
 
     override fun onResume() {
         super.onResume()
