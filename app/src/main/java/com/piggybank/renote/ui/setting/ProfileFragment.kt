f188374ed92c67336b0604ce0122d09241ad656b
Muhammad Fahreza <yya77591@gmail.com>
2024-11-18 00:09:01 +0700

    update SettingFragment

---
+++ app/src/main/java/com/piggybank/renote/ui/setting/ProfileFragment.kt
@@ -0,0 +1,7 @@
+package com.piggybank.renote.ui.setting
+
+import androidx.fragment.app.Fragment
+
+class ProfileFragment : Fragment() {
+
+}
--- app/src/main/java/com/piggybank/renote/ui/setting/SettingFragment.kt
+++ app/src/main/java/com/piggybank/renote/ui/setting/SettingFragment.kt
@@ -5,12 +5,13 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import com.piggybank.renote.R
 import com.piggybank.renote.databinding.FragmentSettingBinding
 
 class SettingFragment : Fragment() {
 
     private var _binding: FragmentSettingBinding? = null
-
     private val binding get() = _binding!!
 
     override fun onCreateView(
@@ -20,6 +21,15 @@ class SettingFragment : Fragment() {
     ): View {
         _binding = FragmentSettingBinding.inflate(inflater, container, false)
         val root: View = binding.root
+
+        // Set click listeners
+        binding.profileOption.setOnClickListener {
+            findNavController().navigate(R.id.action_settingFragment_to_profileFragment)
+        }
+
+        binding.tentangOption.setOnClickListener {
+            findNavController().navigate(R.id.action_settingFragment_to_tentangFragment)
+        }
 
         return root
     }
