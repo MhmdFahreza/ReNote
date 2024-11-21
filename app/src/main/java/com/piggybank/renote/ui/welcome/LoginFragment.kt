b0214c2ed99abe1559175083cee19d7f1afccaf2
Muhammad Fahreza <yya77591@gmail.com>
2024-11-21 23:29:41 +0700

    updated front end

--- app/src/main/java/com/piggybank/renote/ui/welcome/LoginFragment.kt
+++ app/src/main/java/com/piggybank/renote/ui/welcome/LoginFragment.kt
@@ -1,18 +1,40 @@
 package com.piggybank.renote.ui.welcome
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.piggybank.renote.R
+import com.piggybank.renote.ui.main.MainActivity
 
 class LoginFragment : Fragment() {
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-
         return inflater.inflate(R.layout.fragment_login, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        // Set listener untuk grid_slider
+        val gridSlider = view.findViewById<View>(R.id.grid_slider)
+        gridSlider.setOnClickListener {
+            navigateToMainActivity()
+        }
+
+        // Set listener untuk logo_card
+        val logoCard = view.findViewById<View>(R.id.logo_card)
+        logoCard.setOnClickListener {
+            navigateToMainActivity()
+        }
+    }
+
+    private fun navigateToMainActivity() {
+        val intent = Intent(requireContext(), MainActivity::class.java)
+        startActivity(intent)
     }
 }
